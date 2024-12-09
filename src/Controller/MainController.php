98442813ee0f71f4e9a3e346a67b5c1d39b74aef
Esteban BACKES <esteban.backes@gmail.com>
2024-12-09 10:59:07 +0100

    hahahahah

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\CDRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    #[Route('/', name: 'app_main')]
+    public function index(CDRepository $CDRepository): Response
     {
+        $cds = $CDRepository->findAll();
         return $this->render('main/index.html.twig', [
+            'cds' => $cds,
             'controller_name' => 'MainController',
         ]);
     }
