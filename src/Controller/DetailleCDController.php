df9e0f9866351ff48961b2c93ef6b850f04ad174
Fabien Sprocq <sprocq.fabien@gmail.com>
2024-12-17 09:18:11 +0100

    Page detaille cd sans image

--- src/Controller/DetailleCDController.php
+++ src/Controller/DetailleCDController.php
@@ -3,36 +3,38 @@
 namespace App\Controller;
 
 use App\Entity\Cd;
+use App\Repository\CDRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DetailleCDController extends AbstractController
 {
-    #[Route('/cd/details', name: 'cd_details')]
-    public function details(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/cd/details/{id}', name: 'cd_details', requirements: ['id' => '\d+'])]
+    public function details(CDRepository $CDRepository,Request $request, EntityManagerInterface $entityManager): Response
     {
         // Récupérer le paramètre "titre" de la requête
         $titre = $request->query->get('titre');
 
         if (!$titre) {
-            return $this->render('cd/error.html.twig', [
+            return $this->render('detaille_cd/error.html.twig', [
                 'message' => 'Titre non fourni.'
             ]);
         }
 
         // Rechercher le CD dans la base de données
-        $cd = $entityManager->getRepository(Cd::class)->findOneBy(['titre' => $titre]);
+        $cd = $CDRepository->findOneBy(['titre' => $titre]);
 
         if (!$cd) {
-            return $this->render('cd/error.html.twig', [
+            return $this->render('detaille_cd/error.html.twig', [
                 'message' => 'CD introuvable.'
             ]);
         }
 
         // Rendre la vue avec les détails du CD
-        return $this->render('cd/details.html.twig', [
+        return $this->render('detaille_cd/index.html.twig', [
             'cd' => $cd,
         ]);
     }
